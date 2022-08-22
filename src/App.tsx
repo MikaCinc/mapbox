c54dc1ef678d7c05ba75ee75d2fc378979cad9d6
MikaCinc <mihajlo.ls00@outlook.com>
2022-08-22 21:02:53 +0200

    Polygons working

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,52 @@ import "./App.css";
 import { Search } from "./components";
 import { ISearchOption } from "./interfaces";
 /* Tools */
-import Map, { Marker, Popup } from "react-map-gl";
+import Map, { Layer, Marker, Popup, Source } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+
+const defaultPolygon = {
+  type: "Feature" as const,
+  geometry: {
+    type: "Polygon" as const,
+    // These coordinates outline Maine.
+    coordinates: [
+      [
+        [-67.13734, 45.13745],
+        [-66.96466, 44.8097],
+        [-68.03252, 44.3252],
+        [-69.06, 43.98],
+        [-70.11617, 43.68405],
+        [-70.64573, 43.09008],
+        [-70.75102, 43.08003],
+        [-70.79761, 43.21973],
+        [-70.98176, 43.36789],
+        [-70.94416, 43.46633],
+        [-71.08482, 45.30524],
+        [-70.66002, 45.46022],
+        [-70.30495, 45.91479],
+        [-70.00014, 46.69317],
+        [-69.23708, 47.44777],
+        [-68.90478, 47.18479],
+        [-68.2343, 47.35462],
+        [-67.79035, 47.06624],
+        [-67.79141, 45.70258],
+        [-67.13734, 45.13745],
+      ],
+    ],
+  },
+  properties: {},
+};
+
+/* const fillLayer = {
+  id: "mainPolygon",
+  type: "fill",
+  source: "mainPolygon", // reference the data source
+  layout: {},
+  paint: {
+    "fill-color": "#0080ff", // blue color fill
+    "fill-opacity": 0.5,
+  },
+}; */
 
 function App() {
   const [selectedBuilding, setSelectedBuilding] = useState<any>();
@@ -15,6 +59,32 @@ function App() {
   const handleBuildingData = (building: any) => {
     console.log("handleBuildingData", building);
     setSelectedBuilding(building);
+  };
+
+  const getGeomData = () => {
+    if (!selectedBuilding || !selectedBuilding.parcelInfo) {
+      return defaultPolygon;
+    }
+
+    const polygons = selectedBuilding?.parcelInfo?.geom?.coordinates;
+    if (!polygons.length) return defaultPolygon;
+
+    // For now only first polygon is used
+    const coordinatesAdapted = polygons[0]?.coordinates.map((c: any) => [
+      c.longitude,
+      c.latitude,
+    ]);
+
+    console.log(coordinatesAdapted);
+
+    return {
+      type: "Feature" as const,
+      geometry: {
+        type: "Polygon" as const,
+        coordinates: [[...coordinatesAdapted]],
+      },
+      properties: {},
+    };
   };
 
   return (
@@ -31,18 +101,25 @@ function App() {
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
         {selectedBuilding && (
-          <Marker
-            longitude={selectedBuilding.coordinates.longitude}
-            latitude={selectedBuilding.coordinates.latitude}
-            anchor="bottom"
-            onClick={() => {
-              console.log("onClick");
-
-              setShowPopup(true);
-            }}
-          >
-            <img className="marker" src="/mapbox-icon.png" />
-          </Marker>
+          <>
+            <Marker
+              longitude={selectedBuilding.coordinates.longitude}
+              latitude={selectedBuilding.coordinates.latitude}
+              anchor="bottom"
+              onClick={() => setShowPopup(true)}
+            >
+              <img className="marker" src="/mapbox-icon.png" />
+            </Marker>
+            <Source id="mainPolygon" type="geojson" data={getGeomData()}>
+              <Layer
+                paint={{
+                  "fill-color": "#0080ff", // blue color fill
+                  "fill-opacity": 0.5,
+                }}
+                type="fill"
+              />
+            </Source>
+          </>
         )}
         {selectedBuilding && showPopup && (
           <Popup
